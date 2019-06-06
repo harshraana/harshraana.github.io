d60d5645a1c0dde5041a8a197fd56529a405aa5e
KADC\harsh.rana <rharsh1013@gmail.com>
2019-06-06 10:09:15 +0530

    Projects commented

--- js/main.js
+++ js/main.js
@@ -112,7 +112,7 @@ $(document).ready(function () {
         slidesToShow: 3,
         arrows: true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         responsive: [{
                 breakpoint: 768,
                 settings: {
